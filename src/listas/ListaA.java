ceddd1649b2e23fe215ea52e0a74822a1e3bc0a7
SebastianAlba <sebasjosue84@gmail.com>
2018-03-19 00:55:20 -0600

    Lista A
    Se creó la lista de tipo A.

--- src/listas/ListaA.java
+++ src/listas/ListaA.java
@@ -1,5 +1,63 @@
 package listas;
-
+import nodo.*;
 public class ListaA {
-
-}
+	private NodoSimple inicio;
+	private int tamanio;
+	
+	public void ListaA() {
+		inicio = null;
+		tamanio = 0;
+	}
+	public boolean estaVacia() {
+		return inicio == null;
+	}
+	public int getTamanio() {
+		return tamanio;
+	}
+	public void agregarAlFinal(int valor) {
+		NodoSimple nuevo = new NodoSimple();
+		nuevo.setValor(valor);
+		if (estaVacia()) {
+			inicio = nuevo;
+		} else { 
+			NodoSimple aux = inicio;
+			while(aux.getSiguiente() != null) {
+				aux = aux.getSiguiente();}
+			aux.setSiguiente(nuevo);}
+		tamanio ++;}
+	public void agregarAlInicio(int valor) {
+		NodoSimple nuevo = new NodoSimple();
+		nuevo.setValor(valor);
+		if (estaVacia()) {
+			inicio = nuevo;
+			}else {
+				nuevo.setSiguiente(inicio);
+				inicio = nuevo;
+		}
+		tamanio ++;}
+	public void removerPorPosicion(int posicion) {
+		if (posicion >= 0 && posicion < tamanio) {
+			if(posicion ==0) {
+				inicio = inicio.getSiguiente();}
+			else {
+				NodoSimple aux= inicio;
+				for (int i = 0; i< posicion-1 ;i++) {
+					aux = aux.getSiguiente();}
+				NodoSimple siguiente = aux.getSiguiente();
+				aux.setSiguiente(siguiente.getSiguiente());}
+			tamanio --;}}
+	public void vaciarLista() {
+		inicio = null;
+		tamanio = 0;}
+	public void mostrar() {
+		if (!estaVacia()) {
+			NodoSimple aux = inicio;
+			int i = 0;
+			while(aux != null) {
+				System.out.print(i+ ".[" + aux.getValor() + "]" + " -> ");
+				aux = aux.getSiguiente();
+				i ++;}}}
+public void seDestruyeAlJefe() {}
+				}
+				
+			
