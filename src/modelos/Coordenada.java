74593e404d1f3d242d7521ef32c344d68e46a29e
SebastianAlba <sebasjosue84@gmail.com>
2018-03-28 21:52:17 -0600

    Clase Coordenada
    Se creó la clase coordenada

---
+++ src/modelos/Coordenada.java
@@ -0,0 +1,29 @@
+package modelos;
+
+import javax.swing.JOptionPane;
+
+ 
+public class Coordenada {
+   private float x;
+   private float y;
+    
+   public Coordenada(float x, float y){ 
+   this.x=x;this.y=y;}
+   
+   public void setX(float x){this.x=x;}
+ 
+  public void setY(float y){this.y=y;}
+  
+  public float getX( ){return x;}
+ 
+  public float getY( ){return y;}
+  
+  public Coordenada suma(Coordenada c){
+  float sumX=this.x+c.getX();
+  float sumY=this.y+c.getY();
+  
+  Coordenada cor=new Coordenada (sumX,sumY);
+  return cor;
+  
+  }
+}
