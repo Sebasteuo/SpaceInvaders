ee3f0c02765c72871f614a565a8c9eb645d87350
SebastianAlba <sebasjosue84@gmail.com>
2018-03-28 21:56:30 -0600

    Clase Asteroide
    Se creó la clase Asteroide

---
+++ src/modelos/Asteoride.java
@@ -0,0 +1,26 @@
+package modelos;
+ 
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JOptionPane;
+
+public class Asteroide extends Coordenada implements Dibujable{
+
+    int largo;
+    int ancho;
+    Color color;
+    public Asteroide(Coordenada c, int a, int b, Color col){ 
+    super(c.getX(),c.getY()); 
+    largo=a;ancho=b;color=col;
+    }
+    
+ 
+    @Override
+    public void dibujar(Graphics g) { 
+        g.setColor(color);
+        g.fillRect((int)getX(), (int)getY(), largo, ancho); 
+     }
+    
+    
+    
+}
