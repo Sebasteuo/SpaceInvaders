7a7e1800677022d487901440895fa1391e452201
SebastianAlba <sebasjosue84@gmail.com>
2018-03-28 21:55:36 -0600

    Clase Invasor
    Se creó con modificaciones la clase Invasor

--- src/modelos/Invasor.java
+++ src/modelos/Invasor.java
@@ -1,17 +1,40 @@
- 
 package modelos;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.net.URL;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
  
-public class Invasor  extends JLabel{
+public class Invasor  extends Coordenada implements Dibujable{
     
-public Invasor(int x,int y){    
-String path = "/imagenes/Invasor.png";  
-URL url = this.getClass().getResource(path);  
-ImageIcon icon = new ImageIcon(url);  
- setLocation(x,10);
- setSize(200,200);
- setIcon(icon);}}
+   private float radio;
+    private Color color;
+    
+    public Invasor(Coordenada cor,float r,Color c){super(cor.getX(),cor.getY());radio=r;color=c;}
+   
+
+        @Override
+	public void dibujar(Graphics g){    
+      
+	 
+     
+    ImageIcon Img = new ImageIcon(getClass().getResource("/imagenes/Invasor.jpg")); 
+g.drawImage(Img.getImage(), (int)this.getX(), (int)this.getY(), 70, 70, null);
+
+    
+    //String d="\jjkjjj/";g.setColor( Color.YELLOW );
+ 
+  
+
+// g.drawLine(40,20,60, 25);
+
+ 
+    }
+
+
+
+}
